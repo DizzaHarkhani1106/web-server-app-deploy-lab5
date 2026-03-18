89d330b07c36b4323459ba5bddaa42e2b453a21f
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-03-17 20:18:08 -0400

    Add middleware extension method for cleaner registration

---
+++ DH_GlobalExceptionHandler/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using DH_GlobalExceptionHandler.Exceptions;
+using Microsoft.AspNetCore.Http;
+
+namespace DH_GlobalExceptionHandler.Middleware
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var statusCode = HttpStatusCode.InternalServerError;
+            var message = "An unexpected error occurred.";
+
+            switch (ex)
+            {
+                case BadRequestException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    message = ex.Message;
+                    break;
+
+                case UnauthorizedException:
+                    statusCode = HttpStatusCode.Unauthorized;
+                    message = ex.Message;
+                    break;
+
+                case NotFoundException:
+                    statusCode = HttpStatusCode.NotFound;
+                    message = ex.Message;
+                    break;
+
+                case ConflictException:
+                    statusCode = HttpStatusCode.Conflict;
+                    message = ex.Message;
+                    break;
+            }
+
+            var response = new
+            {
+                StatusCode = (int)statusCode,
+                Message = message,
+                ExceptionType = ex.GetType().Name
+            };
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            var json = JsonSerializer.Serialize(response);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
